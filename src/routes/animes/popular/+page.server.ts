322a3ce70db8f49b4cf6dbb380c5eead2ec1dd86
zarqizoubir <zarqi.ezzoubair@etu.uae.ac.ma>
2023-03-16 02:03:26 +0100

    Edit max Anime Card"

--- src/routes/animes/popular/+page.server.ts
+++ src/routes/animes/popular/+page.server.ts
@@ -3,7 +3,8 @@ import type { PageServerLoad } from "../$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
 
-    const popular_animes = await locals.anilist.popular(100)
+    const popular_animes = await locals.anilist.popular(50)
+
     return {
         popular_animes
     }
