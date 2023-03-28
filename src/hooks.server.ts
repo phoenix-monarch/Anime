56a05d06d2115045cf9360b4220fc3cf94a81fed
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-03-28 17:29:21 +0530

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import type { Handle } from "@sveltejs/kit"
 
 
 export const handle:Handle =async ({event,resolve}) => {
-    event.locals.gogo = new GogoAnime()
+    event.locals.gogo = new GogoAnime.mom()
     event.locals.anilist = new Anilist()
     
     const response = await resolve(event)
